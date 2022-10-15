4bbd7bb725ca24eb3a8f1f00489dd34e21f8c55a
19harshal <hlolusare@gmail.com>
2022-10-15 17:59:21 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,17 @@
+class demo{
+
+	public static void m1(){
+		System.out.println("m1");
+	}
+
+<<<<<<< Updated upstream
+	public static void newREq(){
+		System.out.println("new requirement");
+	}
+=======
+	public static void m2(){
+		System.out.println("work in progress");
+	}
+
+>>>>>>> Stashed changes
+}
---
+++ demo.java.bak
@@ -0,0 +1,9 @@
+class demo{
+
+	public static void m1(){
+		System.out.println("m1");
+	}
+
+	
+
+}
